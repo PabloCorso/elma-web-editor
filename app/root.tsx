da4af4a77223551e682973222c7477334ee66be0
Pablo Corso <pabcorso@gmail.com>
2026-08-28 16:30:40 +0200

    Update font load

--- app/root.tsx
+++ app/root.tsx
@@ -11,15 +11,11 @@ import type { Route } from "./+types/root";
 import stylesheet from "./styles/tailwind.css?url";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: "preload",
+    href: "/fonts/inter-latin.woff2",
+    as: "font",
+    type: "font/woff2",
   },
   { rel: "icon", href: "/favicon.ico", sizes: "any" },
   { rel: "manifest", href: "/site.webmanifest" },
